132ded4d815b5defc3af21c63818439e080ca0a0
audubilly <audubilly89@gmail.com>
2021-09-06 21:25:47 +0100

    refactoring

--- src/main/java/io/billy/microservices/ratingsdataservice/models/UserRatings.java
+++ src/main/java/io/billy/microservices/ratingsdataservice/models/UserRatings.java
@@ -1,15 +1,17 @@
 package io.billy.microservices.ratingsdataservice.models;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Ratings {
-    private String movieId;
-    private int rating;
+public class UserRatings{
+
+    private List<Ratings> userRating;
 
 }
--- src/main/java/io/billy/microservices/ratingsdataservice/resource/MovieResource.java
+++ src/main/java/io/billy/microservices/ratingsdataservice/resource/MovieResource.java
@@ -1,16 +1,35 @@
 package io.billy.microservices.ratingsdataservice.resource;
 
 import io.billy.microservices.ratingsdataservice.models.Ratings;
+import io.billy.microservices.ratingsdataservice.models.UserRatings;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/ratingData")
 public class MovieResource {
 
     @RequestMapping("/{movieId}")
     public Ratings getRating(@PathVariable("movieId") String movieId){
-        return new Ratings("123",4);
+        return new Ratings(movieId,4);
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserRatings getUserRating(@PathVariable("userId") String userdId){
+        List<Ratings> ratings = Arrays.asList(
+                new Ratings("1234",4),
+                new Ratings("574",8)
+        );
+        UserRatings userRatings = new UserRatings();
+        userRatings.setUserRating(ratings);
+        log.info("user ratings --{}", userRatings);
+        return userRatings;
     }
 }
